ce06727777ea0251779472e02d8798315e348912
gustavo Almeida <gu.almeidan2007@gmail.com>
2024-10-23 09:12:55 -0300

    sec: encrypted password

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
 const router = express.Router();
 
 // Rota de login
 router.post('/login', async (req, res) => {
     const { username, password } = req.body;
-    const user = await User.findOne({ username, password });
+    const user = await User.findOne({ username });
 
-    if (user) {
-        res.status(200).json({ message: 'Login bem-sucedido! :)', redirect: '/catalog/' }); // Adicionado redirecionamento
+    if (user && await bcrypt.compare(password, user.password)) {
+        res.status(200).json({ message: 'Login bem-sucedido! :)', redirect: '/catalog/' });
     } else {
         res.status(401).json({ message: 'Credenciais inválidas.' });
     }
 });
 
 module.exports = router;
-
